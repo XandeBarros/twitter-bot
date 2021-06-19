ada36725c65261297cbd50febe50c06845631b89
XandeBarros <barrosalex6@gmail.com>
2021-06-19 19:28:57 -0400

    Create Api and return to project

---
+++ config.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import tweepy
+import logging
+import os
+
+logger = logging.getLogger()
+
+def create_api():
+  '''Credentials'''
+
+  api_key = os.getenv("API_KEY")
+  api_secret = os.getenv("API_SECRET")
+  access_token = os.getenv("ACCESS_TOKEN")
+  access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+  auth = tweepy.OAuthHandler(api_key, api_secret)
+  auth.set_access_token(access_token, access_token_secret)
+
+  api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+  try:
+      api.verify_credentials()
+  except Exception as e:
+      logger.error("Error creating API", exc_info=True)
+      raise e
+  logger.info("API created")
+  return api
