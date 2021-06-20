e0dfa596da35fdb90c0054d31eec6df0da0fd3d0
XandeBarros <barrosalex6@gmail.com>
2021-06-19 23:09:28 -0400

    Formating Date to tweet

---
+++ data.py
@@ -0,0 +1,14 @@
+import datetime
+
+def date():
+  dt = datetime.datetime.today()
+  
+  day = dt.day
+  month = dt.month
+  year = dt.year
+
+  monthArray= ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+
+  result = f"Hoje é {day} de {monthArray[month-1]} de {year}."
+
+  return result
