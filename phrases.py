e52a83064005537055f1f8ded57a0f8fc3721e44
XandeBarros <barrosalex6@gmail.com>
2021-06-20 12:42:00 -0400

    phrases database ~some day I will transform this is an API~

---
+++ phrases.py
@@ -0,0 +1,7 @@
+def get_phrases():
+  listPhrase = list((
+    "E vale lembrar: Não dê palco pra maluco ;)",
+    "E vale lembrar: No EAD todas as ativadades são em grupo ;)"
+  ))
+
+  return listPhrase
