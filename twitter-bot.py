b94cb0e25f349fda39620909d8209d55ac854293
XandeBarros <barrosalex6@gmail.com>
2021-06-20 00:42:40 -0400

    adding a multiprocessing tasks to the bot

--- twitter-bot.py
+++ twitter-bot.py
@@ -1,31 +1,50 @@
 import tweepy
 import logging
+import time
+import multiprocessing
 from config import create_api
 from weather import get_weather
 from data import date
-import time
 
 HALF_DAY = 43200
+FIVE_MINUTES = 300
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 def postTweet(api, text):
-  api.update_status(text)
-  logger.info("Posting...")
+  while True:
+    logger.info("Posting...")
+    api.update_status(text)
+    logger.info("Posted ;)") 
+    time.sleep(HALF_DAY)
+
+def likeEveryNewTweet(api, user):
+  while True:
+    logger.info("Searching new tweets...")
+    tweets = api.user_timeline(user, exclude_replies=True)
+
+    for status in tweets:
+      api.create_favorite(status.id)
+      logger.info("Favorited")
+      time.sleep(FIVE_MINUTES)
+      logger.info("Wainting")
 
 def main():
   api = create_api()
   weatherStatus = get_weather()
   dataString = date()
 
-  tweet = f"{dataString} {weatherStatus} E vale lembrar: Não dê palco pra maluco ;)"
+  user = "XDessau"
+  tweet = f"{dataString} {weatherStatus} E vale lembrar: No EAD todas as ativadades são em grupo ;)"
 
-  while True:
-    postTweet(api, tweet)
-    logger.info("Até amanhã ;)") 
-    time.sleep(HALF_DAY)
-
-
+  p1 = multiprocessing.Process(target=postTweet, args=[api, tweet])
+  p2 = multiprocessing.Process(target=likeEveryNewTweet, args=[api, user])
+  
+  p1.start()
+  p2.start()
+  p1.join()
+  p2.join()
+    
 if __name__ == "__main__":
   main()
