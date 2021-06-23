2e6b8a2323c4e7e97b5d1e52096dc677639f054d
XandeBarros <barrosalex6@gmail.com>
2021-06-23 00:12:04 -0300

    new phrase ayyyye funnnn

--- phrases.py
+++ phrases.py
@@ -2,7 +2,8 @@ def get_phrases():
   listPhrase = list((
     "E vale lembrar: Não dê palco pra maluco ;)",
     "E vale lembrar: No EAD todas as ativadades são em grupo ;)",
-    "E vale lembrar: O início é sempre a parte mais difícil, afinal o atrito estático é maior que o atrito dinâmico"
+    "E vale lembrar: O início é sempre a parte mais difícil, afinal o atrito estático é maior que o atrito dinâmico",
+    "E vale lembrar: Se ele/ela for muita areia pro seu caminhãozinho, faça quantas viagens forem necessárias"
   ))
 
   return listPhrase
